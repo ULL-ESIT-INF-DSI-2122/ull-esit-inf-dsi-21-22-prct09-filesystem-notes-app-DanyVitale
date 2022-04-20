a8d5d81b90d8982b5a81f8166ac7208ebc145e93
Daniele Vitale <alu0101329017@ull.edu.es>
2022-04-20 21:13:33 +0000

    Testing

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Add describes the addition of two numbers.
- * @param a is the first number.
- * @param b is the second number.
- * @returns is the sum of a and b.
- */
-export function add(a: number, b: number): number {
-  return a + b;
-}
