30922cf6e5a8d2783992894f45a6028ed15646af
Daniele Vitale <alu0101329017@ull.edu.es>
2022-04-20 18:14:04 +0000

    Test Actions

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Add is a function that adds two numbers together.
- * @param a is a number
- * @param b is a number
- * @returns Returns the sum of a and b
- */
-export function add(a: number, b: number) {
-  return a + b;
-}
