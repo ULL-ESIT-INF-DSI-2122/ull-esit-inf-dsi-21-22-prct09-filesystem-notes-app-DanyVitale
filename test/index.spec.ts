27644ac62ce11c143947bc4f6d36348f7a940c57
Daniele Vitale <alu0101329017@ull.edu.es>
2022-04-20 23:26:05 +0000

    SC testing

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,11 +1,13 @@
 import 'mocha';
 import {expect} from 'chai';
-import {add} from '../src/index';
+import {add, subtract} from '../src/index';
 
 describe('add', () => {
   it('should add two numbers', () => {
     expect(add(1, 2)).to.equal(3);
   });
 
-
+  it('should subtract two numbers', () => {
+    expect(subtract(3, 2)).to.equal(1);
+  });
 });
